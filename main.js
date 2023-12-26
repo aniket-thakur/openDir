6b6bb4f64e1325f327ff5de3caba38a9f2d5cc3a
Aniket Thakur <anikett126@gmail.com>
2023-12-26 22:53:20 +0530

    edit/delete by id

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
     let divContainer= document.querySelector('#container');
     let myTemplates = document.querySelector('#myTemplates');
     let fid = 0;
+    let folders = [];  // to store the name and fid of every  folder
 
     btn.addEventListener('click', function(){
         let fname = prompt('Enter folder name:');
@@ -18,9 +19,13 @@
         // delete a folder(Using closure )
         let delFolder = divFolder.querySelector("[action = 'del']");
         delFolder.addEventListener("click",function(){
-            let flag = confirm(`${fname} will be removed!`);
-            if(flag == true){    
-                divContainer.removeChild(divFolder);
+            let flag = confirm(`${divName.innerHTML} will be removed!`);
+            if(flag == true){   
+                divContainer.removeChild(divFolder); 
+                let idx = folders.findIndex(f => f.id == parseInt(divFolder.getAttribute('fid')));
+                console.log(idx);
+                folders.splice(idx , 1);
+                console.log(folders);
             }
             
         });
@@ -31,9 +36,17 @@
             if(!fName2){
                 return;
             }
-            divName.innerHTML =fName2;
+            divName.innerHTML = fName2;
+            let folder = folders.find(f => f.id == parseInt(divFolder.getAttribute('fid')));
+            folder.name = fName2; // chage the edited name in the array also
+            console.log(folders);
         });
-        divContainer.appendChild(divFolder);
+        divContainer.appendChild(divFolder);  // adding folder in the container
+        folders.push({
+            name : fname,
+            id : fid
+        });
+        console.log(folders);
     });
     
 })
